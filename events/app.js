40601b6f03912193c598428a16058f0b75ecf60f
nelson <npedrosapena@danielcastelao.org>
2015-08-02 12:36:31 +0200

    eventos de ratón. ng-click

--- events/app.js
+++ events/app.js
@@ -2,12 +2,13 @@ angular.module('app',[])
 
     .controller("controladorBasico",['$scope',function($scope){
 
-        $scope.contador=0;
-        $scope.add=function(v){
-            $scope.contador+=v;
+        $scope.contador=0; //inicializamos la variable contador de la web a 0
+
+        $scope.add=function(v){ //asignamos esta función a la variable add que definimos en el html
+            $scope.contador+=v; //sumamos al valor de la variable definida en el html, el número V que pasamos por la función
         };
 
-        $scope.substract=function(v){
-            $scope.contador-=1;
+        $scope.substract=function(v){ //asignamos esta función a la variable add que definimos en el html
+            $scope.contador-=v; //restamos al valor de la variable definida en el html, el número V que pasamos por la función
         }
     }]);
